d86bd5092e3ba4ffd3b93c202667c4ae1cd31bcc
Linde Christian <linde.chris@googlemail.com>
2018-02-07 19:24:04 +0100

    update mp4 quality tag

--- zdfLoad.sh
+++ zdfLoad.sh
@@ -10,18 +10,20 @@ else
   exit 1
 fi
 
-wget "$baseURL" -O film.html
+wget -v "$baseURL" -O film.html
 apiToken=$(grep -A13 -e "data-zdfplayer-jsb" film.html | grep -e "apiToken" | sed "s/.*apiToken\": \"\(.*\)\".*/\1/")
 nextUrl=$(grep -e "\.json" film.html | grep "profile=player" | sed "s/.*content\": \"\(.*\)\".*/\1/")
-rm film.html
 
-wget --header="Api-Auth:Bearer $apiToken" "$nextUrl" -O film.json
+echo "$apiToken - $nextUrl"
+wget -v --header="Api-Auth:Bearer $apiToken" "$nextUrl" -O film.json
 nextUrl=$(sed "s/.*ptmd\":\"\([^\"]*\)\".*/\1/" film.json | sed "s/\\\//g")
-rm film.json
 
-wget --header="Api-Auth:Bearer $apiToken" "https://api.zdf.de$nextUrl" -O apiTex.html
-filmURL=$(grep "\.mp4" apiTex.html | grep "1456k" | tail -n1 | sed "s/.*uri\" : \"\(.*\)\".*/\1/")
-rm apiTex.html
+echo "https://api.zdf.de$nextUrl"
+wget -v --header="Api-Auth:Bearer $apiToken" "https://api.zdf.de$nextUrl" -O apiTex.html
+filmURL=$(grep "\.mp4" apiTex.html | grep "14" | tail -n1 | sed "s/.*uri\" : \"\(.*\)\".*/\1/")
 
 wget "$filmURL" -O "$outputFile"
 #
+rm film.json
+rm apiTex.html
+rm film.html
